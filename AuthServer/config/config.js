04dd8c9c923d3b9f19490846d87858cc23935f44
KJY <KJY@DESKTOP-ULPJSQS>
2017-07-02 05:17:56 +0900

    port number 3000 -> 3002 changed

--- AuthServer/config/config.js
+++ AuthServer/config/config.js
@@ -4,7 +4,7 @@
  */
 
 module.exports = {
-	server_port: 3000,
+	server_port: 3002,
 	db_url: 'mongodb://localhost:27017/shopping',
 	db_schemas: [
 	    {file:'./user_schema', collection:'users6', schemaName:'UserSchema', modelName:'UserModel'}
